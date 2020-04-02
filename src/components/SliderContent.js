ccce4fa9b02382d8b049a5e5f0826c0f42c37a1d
EloK6 <ecassignol@gmail.com>
2020-04-02 17:56:52 +0200

    add emotion for handle css

--- src/components/SliderContent.js
+++ src/components/SliderContent.js
@@ -1,15 +1,26 @@
-import React from "react";
+// import React from "react";
 
-const SliderContent = () => (
-  <div
-    style={{
-      transform: `translateX(-${props => props.translate}px)`,
-      transition: `transform ease-out ${props => props.transition}s`,
-      height: "100%",
-      width: `${props => props.width}px`,
-      display: "flex"
-    }}
-  ></div>
-);
+// const SliderContent = () => (
+//   <div
+//     style={{
+//       transform: `translateX(-${props => props.translate}px)`,
+//       transition: `transform ease-out ${props => props.transition}s`,
+//       height: "100%",
+//       width: `${props => props.width}px`,
+//       display: "flex"
+//     }}
+//   ></div>
+// );
 
+// export default SliderContent;
+
+import styled from "@emotion/styled";
+
+const SliderContent = styled.div`
+  transform: translateX(-${props => props.translate}px);
+  transition: transform ease-out ${props => props.transition}s;
+  height: 100%;
+  width: ${props => props.width}px;
+  display: flex;
+`;
 export default SliderContent;
